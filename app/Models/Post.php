3368497a1f31a1e9481509ca4f7f458da6b39461
Andrean <andrean@gmail.com>
2024-04-08 04:24:29 +0800

    feat(TICKET-1): backpack for user post and tag

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
